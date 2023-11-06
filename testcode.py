cad30b6c0c192cf216c4f7a3088b10d8a0666be8
j-m-rodriguez <jrodriguez25225@gmail.com>
2023-11-06 02:49:49 -0800

    updated project so that it loops through all products in a page, but still missing title

--- testcode.py
+++ testcode.py
@@ -5,10 +5,20 @@ import re
 main_url = "https://books.toscrape.com/"
 r = requests.get(main_url)
 soup = BeautifulSoup(r.text, 'html.parser')
-
-print(soup.find(class_="col-xs-6 col-sm-4 col-md-3 col-lg-3").a['href'])
-print(soup.find('h3').a['href'])
-
+'''
+# loops through each product by the class and retrieves product link
 for link in soup.find_all(class_="col-xs-6 col-sm-4 col-md-3 col-lg-3"):
-    product_link = main_url + link.a['href']
-    print(product_link)
+    product_page_url = main_url + link.a['href']
+    print(product_page_url)
+    r_product = requests.get(product_page_url)
+    product_soup = BeautifulSoup(r_product.text, 'html.parser')
+print(book_soup.find_all('td')[0].contents[0])
+print(book_soup.find_all('td')[2].contents[0])
+'''
+book_url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+r_book = requests.get(book_url)
+book_soup = BeautifulSoup(r_book.text, 'html.parser')
+
+img_location = str((book_soup.find(class_="item active").contents[1]))
+img = re.split(r'(media.*jpg)', img_location)
+print(main_url + img[1])
