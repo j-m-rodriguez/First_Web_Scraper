9b1e04ea233d04436be1ed7962614545139cbd46
j-m-rodriguez <jrodriguez25225@gmail.com>
2023-11-20 11:06:57 -0800

    went through everything with new mentor. removed list/dictionary redundancy by adding product information directly to dictionary. changed class search for product url to something more reliable. organized structure by moving download image towards the end. fixed link to next page

--- allcategories.py
+++ allcategories.py
@@ -14,6 +14,7 @@ header = ['product_page_url', 'universal_product_code',
           'product_description', 'category',
           'review_rating', 'image_url'
           ]
+product_info = {}
 # setting up directory for CSV files and images
 absolute_path = os.path.dirname(__file__)
 relative_path = "Rodriguez_Jonathan_2_data_images_112023"
@@ -26,10 +27,9 @@ category_links = main_soup.find(class_="nav nav-list").ul
 for link in category_links.find_all('a'):
     # obtain url for the category
     category_url = main_url + link['href']
-    print(category_url)
     # obtain category name
     category = ' '.join(list(filter(None, re.split(r'\s|\\n', link.contents[0]))))
-    print(category)
+    product_info['category'] = category
     # csv file directory
     csv_dir = os.path.join(full_path, "data") + "\\"
     if not os.path.exists(csv_dir):
@@ -51,74 +51,51 @@ for link in category_links.find_all('a'):
     # variable for image naming
     i = 0
     while True:
-        for book in soup.find_all(class_="col-xs-6 col-sm-4 col-md-3 col-lg-3"):
+        # switched to class=product_pod
+        for book in soup.find_all(class_="product_pod"):
             product_section = list(filter(None, re.split(r'\.\./', book.a['href'])))
             product_page_url = main_url + "catalogue/" + product_section[0]
-            print(product_page_url)
-            book_title = book.img['alt']
-            print(book_title)
+            product_info['product_page_url'] = product_page_url
+            product_info['book_title'] = book.img['alt']
             image_url = main_url + book.img['src']
-            print(image_url)
-            img_data = requests.get(image_url).content
-            with open(image_dir + 'image_' + str(i) + '.jpg', 'wb') as handler:
-                handler.write(img_data)
-            i += 1
+            product_info['image_url'] = image_url
+
             r_product = requests.get(product_page_url)
             product_soup = BeautifulSoup(r_product.text.encode('latin1').decode('utf-8'), 'html.parser')
             # product info table
             product_values = product_soup.find_all('td')
-            print(product_values)
 
             # product upc, prices, and quantity
-            universal_product_code = product_values[0].contents[0]
-            print(universal_product_code)
-            price_excluding_tax = product_values[2].contents[0]
-            print(price_excluding_tax)
-            price_including_tax = product_values[3].contents[0]
-            print(price_including_tax)
-            quantity_available = product_values[5].contents[0]
-            print(quantity_available)
+            product_info['universal_product_code'] = product_values[0].contents[0]
+            product_info['price_excluding_tax'] = product_values[2].contents[0]
+            product_info['price_including_tax'] = product_values[3].contents[0]
+            product_info['quantity_available'] = product_values[5].contents[0]
             # get product description
-            product_description = product_soup.article.contents[7].contents[0]
-            print(product_description)
-            # get category by navigating down contents of <ul> tree
-            '''
-            # got category earlier
-            category = product_soup.ul.contents[5].contents[1].contents[0]
-            print(category)
-            '''
+            product_info['product_description'] = product_soup.article.contents[7].contents[0]
+
             # locate and print review rating
             rating_location = product_soup.find(class_="star-rating")
-            review_rating = rating_location['class'][1]
-            print(review_rating)
-            # create list of all product details
-            product = list()
-            product.append(product_page_url)
-            product.append(universal_product_code)
-            product.append(book_title)
-            product.append(price_including_tax)
-            product.append(price_excluding_tax)
-            product.append(quantity_available)
-            product.append(product_description)
-            product.append(category)
-            product.append(review_rating)
-            product.append(image_url)
-            print(product)
-            # dictionary of everything
-            header_product = {header[i]: product[i] for i in range(len(header))}
-            print(header_product)
+            product_info['review_rating'] = rating_location['class'][1]
+
+            print(product_info)
+
+            img_data = requests.get(image_url).content
+            with open(image_dir + 'image_' + str(i) + '.jpg', 'wb') as handler:
+                handler.write(img_data)
+            i += 1
+
+            writer = csv.DictWriter(f, fieldnames=header)
             try:
                 f = open(csv_file, 'a', newline='')
-                writer = csv.DictWriter(f, fieldnames=header)
-                writer.writerow(header_product)
+                writer.writerow(product_info)
+
             except UnicodeEncodeError:
                 f = open(csv_file, 'a', newline='', encoding='utf-16')
-                writer = csv.DictWriter(f, fieldnames=header)
-                writer.writerow(header_product)
+                writer.writerow(product_info)
+
         try:
-            next_page = re.sub(r"/.*?\.html", "/", category_url) + soup.find(class_="next").a['href']
-            print(next_page)
+            next_page = re.sub(r"[a-z]*?\.html", soup.find(class_="next").a['href'], category_url)
             r = requests.get(next_page)
-            soup_category = BeautifulSoup(r.text.encode('latin1').decode('utf-8'), 'html.parser')
-        except Exception:
+            soup = BeautifulSoup(r.text.encode('latin1').decode('utf-8'), 'html.parser')
+        except AttributeError:
             break
